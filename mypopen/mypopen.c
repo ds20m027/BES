1684d10c0c4fe91c12e2e5432f86cdd39050780d
ic17b087 <ic17b087@technikum-wien.at>
2018-04-27 16:53:02 +0200

    changes mypopen

--- mypopen/mypopen.c
+++ mypopen/mypopen.c
@@ -1,40 +1,65 @@
 #include "mypopen.h"
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <errno.h>
 
 FILE* mypopen(const char* command, const char* type) {
 	
 	int pipefd[2];
+	int new_pipefd[2];
 	pid_t cpid;
 	char buf;
 
-	assert(argc == 2);
-
 	if (pipe(pipefd) == -1) {
-		perror("pipe");
-		exit(EXIT_FAILURE);
+		return NULL; /*errno wird schon innerhalb von pipe zugewiesen*/
+	}
+
+	if (cpid >= 0) { /*überprüft ob nicht schon ein Child Prozess läuft*/
+		errno = EAGAIN;
+		return NULL;
 	}
 
 	cpid = fork();
+	
 	if (cpid == -1) {
-		perror("fork");
-		exit(EXIT_FAILURE);
+		return NULL; /*errno wird schon innerhalb von pipe zugewiesen*/
 	}
 
-	if (cpid == 0) {    /* Child reads from pipe */
-		close(pipefd[1]);          /* Close unused write end */
+	if (cpid == 0) {    /* Child*/
+		dup2(pipefd, new_pipefd); /* Creates copy pipefd to new_pipefd */
+
+		if (strcmp(*type, 'r') == 0) {
+			close(pipefd[1]);	/* Close unused write end */
+		}
+
+		if (strcmp(*type, 'w') == 0) {
+			close(pipefd[0]);	/* Close unused read end */
+		}
+
+		else {
+			errno = EINVAL; /*invalid value*/
+			return NULL;
+		}
+		
 
 		while (read(pipefd[0], &buf, 1) > 0)
 			write(STDOUT_FILENO, &buf, 1);
 
 		write(STDOUT_FILENO, "\n", 1);
 		close(pipefd[0]);
-		_exit(EXIT_SUCCESS);
+		exit(EXIT_SUCCESS);
 
 	}
-	else {            /* Parent writes argv[1] to pipe */
+	else {            /* Parent */
 		close(pipefd[0]);          /* Close unused read end */
 		write(pipefd[1], argv[1], strlen(argv[1]));
 		close(pipefd[1]);          /* Reader will see EOF */
 		wait(NULL);                /* Wait for child */
 		exit(EXIT_SUCCESS);
 	}
+}
+
+int mypclose(FILE* stream) {
+	pid_t wait_pid;
 }
--- mypopen/mypopen.h
+++ mypopen/mypopen.h
@@ -2,11 +2,6 @@
 
 #define _MYPOPEN_H_
 
-#include <stdio.h>
-#include <unistd.h>
-#include <sys/wait.h>
-#include <errno.h>
-
 FILE *mypopen(const char *command, const char *type);
 int mypclose(FILE *stream);
 
